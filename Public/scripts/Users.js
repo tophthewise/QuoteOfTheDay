a0404a591c8d1639ee93b1708295dbf3a532a875
tophthewise <eric.porter820@gmail.com>
2026-05-09 20:19:40 -0400

    add objects to send

--- Public/scripts/Users.js
+++ Public/scripts/Users.js
@@ -1,7 +1,12 @@
 let submitForm2  = document.querySelector("#loginForm");
 
 submitForm2.addEventListener("submit", displayInput);
-
+function User(firstname, lastname, username, password) {
+  this.firstname = firstname;
+  this.lastname = lastname;
+  this.username = username;
+  this.password = password;
+}
 function displayInput(e) {
 console.log("Button was clicked");
   const username = document.getElementById("username").value;
@@ -15,6 +20,8 @@ console.log("Button was clicked");
   const lastname = document.getElementById("lastname").value;
     const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
+  const user = new User(firstname, lastname, username, password);
+  console.log(user);
   document.getElementById("displayInfo2").innerHTML += `<span style="color: white;"> First Name: ${firstname}, Last Name: ${lastname}, Username: ${username}, Password: ${password}</span>`;
   e.preventDefault();
 }
